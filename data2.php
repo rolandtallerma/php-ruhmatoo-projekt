254a9be7bc1a9ecc63706be92768804f5116f9cd
rolandtallerma <rolandtallerma@users.noreply.github.com>
2016-12-01 11:31:57 +0200

    Parandasin data2 encodingut

--- data2.php
+++ data2.php
@@ -35,57 +35,57 @@ if (isset($_GET["logout"])) {
 		<select>
 	<option value="TLL">Tallinn</option>
 	<option value="TRT">Tartu</option>
-	<option value="VRU">VĆµru</option>
-	<option value="PRN">PĆ¤rnu</option>
+	<option value="VRU">Võru</option>
+	<option value="PRN">Pärnu</option>
 	<option value="NR">Narva</option>
-	<option value="KJ">Kohtla-JĆ¤rve</option>
+	<option value="KJ">Kohtla-Järve</option>
 	<option value="VLJ">Viljandi</option>
 	<option value="RKR">Rakvere</option>
-	<option value="SLM">SillamĆ¤e</option>
+	<option value="SLM">Sillamäe</option>
 	<option value="KRS">Kuressaare</option>
 	<option value="VLG">Valga</option>
-	<option value="JHI">JĆµhvi</option>
+	<option value="JHI">Jõhvi</option>
 	<option value="HPS">Haapsalu</option>
 	<option value="KEI">Keila</option>
 	<option value="PAI">Paide</option>
-	<option value="TRI">TĆ¼ri</option>
+	<option value="TRI">Türi</option>
 	<option value="TPA">Tapa</option>
-	<option value="PLV">PĆµlva</option>
-	<option value="KVĆ•">KiviĆµli</option>
+	<option value="PLV">Põlva</option>
+	<option value="KVÕ•">Kiviõli</option>
 	<option value="ELV">Elva</option>
 	<option value="SAU">Saue</option>
-	<option value="JGA">JĆµgeva</option>
+	<option value="JGA">Jõgeva</option>
 	<option value="RPL">Rapla</option>
-	<option value="PLT">PĆµltsamaa</option>
+	<option value="PLT">Põltsamaa</option>
 	<option value="PLD">Paldiski</option>
 	<option value="SND">Sindi</option>
 	<option value="KND">Kunda</option>
-	<option value="KRD">KĆ¤rdla</option>
+	<option value="KRD">Kärdla</option>
 	<option value="KRA">Kehra</option>
 	<option value="LKS">Loksa</option>
-	<option value="RPN">RĆ¤pina</option>
-	<option value="TRV">TĆµrva</option>
-	<option value="NRJ">Narva-JĆµesuu</option>
+	<option value="RPN">Räpina</option>
+	<option value="TRV">Tõrva</option>
+	<option value="NRJ">Narva-Jõesuu</option>
 	<option value="TMS">Tamsalu</option>
-	<option value="OTP">OtepĆ¤Ć¤</option>
-	<option value="KIL">Killingi-NĆµmme</option>
+	<option value="OTP">Otepää</option>
+	<option value="KIL">Killingi-Nõmme</option>
 	<option value="KNU">Karksi-Nuia</option>
 	<option value="LIH">Lihula</option>
 	<option value="MTV">Mustvee</option>
-	<option value="VHM">VĆµhma</option>
+	<option value="VHM">Võhma</option>
 	<option value="ANT">Antsla</option>
 	<option value="ABP">Abja-Paluoja</option>
-	<option value="PSS">PĆ¼ssi</option>
+	<option value="PSS">Püssi</option>
 	<option value="SRJ">Suure-Jaani</option>
 	<option value="KLL">Kallaste</option>
-	<option value="MSK">MĆµisakĆ¼la</option>
+	<option value="MSK">Mõisaküla</option>
 	
 </select>
 	<input type="submit" value="vali">
 	
 <br><br>
 
-	    <h1> Vali SĆ¶Ć¶gikoht</h>
+	    <h1> Vali Söögikoht</h>
 		<br><br>
 		
 		<select>
@@ -102,6 +102,11 @@ if (isset($_GET["logout"])) {
 </select>
 	<input type="submit" value="vali">
 	
-</body>
-			
-</html>	
+
+	
+
+	
+
+	
+	</body>
+</html>
