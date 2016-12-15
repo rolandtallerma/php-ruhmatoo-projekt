e85dea9ca7279e1a15b0975fda9e6a613249b824
rolandtallerma <rolandtallerma@users.noreply.github.com>
2016-12-15 13:12:58 +0200

    Fondi suurused

--- data2.php
+++ data2.php
@@ -8,16 +8,28 @@
 <!DOCTYPE html>
 <html>
 	
-		
 	<head>
-		<title>Data2 leht</title>
-		
-		
-		
-	</head>
+	<title>Data2 leht</title>
+		  <font size="3"'." face='Times New Roman'>
+		  <meta charset="utf-8">
+		  <meta name="viewport" content="width=device-width, initial-scale=1">
+		  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+		  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+		  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+		</head>	
+		<style>
+			button {
+				font-size: 15px;
+			}
+			h1 {
+				font-size: 24px;
+			}
+			select.first {
+				font-size: 24px;
+			}
+		</style>
 	<body>
-<!-- joosep/ Lisasin siia prooviks asjad-->
-		<font size="3"'." face='Times New Roman'>  <!--joosep/ Lisasin siia prooviks selle rea-->
+
 			<br><br>
 			
 			<a href="?logout=1"><button>Logi välja</button></a>
@@ -25,21 +37,14 @@
 			
 <br><br>   
 	<!--/joosep Lisasin siia prooviks siit vahemikust-->
-	<head>
-  <font size="3"'." face='Times New Roman'>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-</head>	
+
 
 <br><br>	
 <!--/joosep  siiani-->  
   <h1> Vali linn</h>
 		<br><br>
 		
-		<select>
+		<select class="first">
 	<option value="TLL">Tallinn</option>
 	
 	
@@ -48,10 +53,10 @@
 	
 <br><br>
 
-	    <h1> Vali Söögikoht</h>
+	    <h1> Vali Söögikoht</h1>
 		<br><br>
 		
-		<select>
+		<select class="first">
 	<option value="KAHT">Korsten, Armastus & Hea Toit </option>
 	<option value="LBRA">Leib Resto ja Aed</option>
 	<option value="MSHA">Masha</option>
