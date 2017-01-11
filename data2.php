6f8b9c44dbcb5f45a06cd8537c5f5866b5803fca
rolandtallerma <rolandtallerma@users.noreply.github.com>
2017-01-11 18:49:50 +0200

    updated data 2

--- data2.php
+++ data2.php
@@ -1,66 +1,23 @@
-<?php 
-	// et saada ligi sessioonile
+<?php
 	require("functions.php");
-	if(!isset ($_SESSION["userId"])) {
-		header("Location: data.php");
-		exit();
-	}
-	
-	//kas kasutaja tahab välja logida
-	// kas aadressireal on logout olemas
 	if (isset($_GET["logout"])) {
 		session_destroy();
 		header("Location: data.php");
-		exit();
 	}
-	
-
-	if (	isset($_POST["arvamus"]) && 
-			!empty($_POST["arvamus"])
-	) {
-		
-		$note = $Helper->cleanInput($_POST["arvamus"]);
-		
-		$Note->saveNote($arvamus);
-		
-	}
-
 ?>
-
-<h1>Data</h1>
-<p>
-	Tere tulemast <a href="user.php"><?=$_SESSION["userEmail"];?></a>!
-	<a href="?logout=1">Logi välja</a>
-</p>
-<h2><i>Märkmed</i></h2>
-<h2>&lt;i&gt;Märkmed&lt;/i&gt;</h2>
-<form method="POST">
-			
-	<label>Märkus</label><br>
-	<input name="note" type="text">
-=======
+<!DOCTYPE html>
+<html>
+	
+		
 	<head>
-	<title>Data2 leht</title>
-		  <font size="3"'." face='Times New Roman'>
-		  <meta charset="utf-8">
-		  <meta name="viewport" content="width=device-width, initial-scale=1">
-		  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-		  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-		  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-		</head>	
-		<style>
-			button {
-				font-size: 15px;
-			}
-			h1 {
-				font-size: 24px;
-			}
-			select.first {
-				font-size: 24px;
-			}
-		</style>
+		<title>Data2 leht</title>
+		
+		
+		
+	</head>
 	<body>
-
+<!-- joosep/ Lisasin siia prooviks asjad-->
+		<font size="3"'." face='Times New Roman'>  <!--joosep/ Lisasin siia prooviks selle rea-->
 			<br><br>
 			
 			<a href="?logout=1"><button>Logi välja</button></a>
@@ -68,37 +25,33 @@
 			
 <br><br>   
 	<!--/joosep Lisasin siia prooviks siit vahemikust-->
-
+	<head>
+  <font size="3"'." face='Times New Roman'>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>	
 
 <br><br>	
 <!--/joosep  siiani-->  
   <h1> Vali linn</h>
 		<br><br>
 		
-		<select class="first">
+		<select>
 	<option value="TLL">Tallinn</option>
->>>>>>> e85dea9ca7279e1a15b0975fda9e6a613249b824
 	
-	<br><br>
 	
-				
+</select>
+	<button type="button" class="btn btn-primary btn-sm">Vali</button> <!--joosep/ Lisasin siia prooviks selle rea-->
 	
-	<input type="submit" value="Jäta Tagasiside">
+<br><br>
 
-</form>
-
-
-
-
-
-<h2 style="clear:both;">Arvamused</h2>
-
-<<<<<<< HEAD
-=======
-	    <h1> Vali Söögikoht</h1>
+	    <h1> Vali Söögikoht</h>
 		<br><br>
 		
-		<select class="first">
+		<select>
 	<option value="KAHT">Korsten, Armastus & Hea Toit </option>
 	<option value="LBRA">Leib Resto ja Aed</option>
 	<option value="MSHA">Masha</option>
@@ -112,7 +65,11 @@
 </select>
 	<button type="button" class="btn btn-primary btn-sm">Vali</button> <!--joosep/ Lisasin siia prooviks selle rea-->
 	
->>>>>>> e85dea9ca7279e1a15b0975fda9e6a613249b824
 
+	
 
+	
 
+	
+	</body>
+</html>
