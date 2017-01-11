b473b818cfac8ae5244d54fc7a52be59b52a1c0b
Paul Vasemagi <vaskpaul@users.noreply.github.com>
2017-01-11 21:11:30 +0200

    spot tagasi korda

--- spot.php
+++ spot.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php 
 
 	require("functions.php");
@@ -56,58 +55,3 @@
 	
 	
 	<a href="?tagasi=1"><button>Tagasi</button></a>
-
-=======
-<?php 
-
-	require("functions.php");
-	if(!isset ($_SESSION["userId"])) {
-		header("Location: login.php");
-		exit();
-	}
-	if (isset($_GET["logout"])) {
-		session_destroy();
-
-		header("Location: login.php");
-		exit();
-	}
-	
-	if (	isset($_POST["nimi"]) && 
-			isset($_POST["tagasiside"]) && 
-			!empty($_POST["nimi"]) && 
-			!empty($_POST["tagasiside"]) 
-	) {
-		
-		$nimi = cleanInput($_POST["nimi"]);
-		$tagasiside = cleanInput($_POST["tagasiside"]);
-		
-		Spot($_POST["nimi"], $_POST["tagasiside"]);
-		
-	}
-	$spot = getSpot();
-
-?>
-
-
-<h1>Spot</h1>
-<p>
-	Tere tulemast <a href="user.php"><?=$_SESSION["userEmail"];?></a>!
-	<a href="?logout=1">Logi välja</a>
-</p>
-<h2><i>Tagasiside</i></h2>
-<form method="POST">
-			
-	<label>Nimi</label><br>
-	<input name="nimi" type="text">
-	
-	<br><br>
-	
-	<label>Tagasiside</label><br>
-	<input name="tagasiside" type="text" input style="height:200px">
-				
-	<br><br>
-	
-	<input type="submit">
-
->>>>>>> 30301b0e3b9b85c998dc036ddf55f19a9f7a7def
-</form>
