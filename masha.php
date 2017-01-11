fc6d9dc8a0ac2e1db5569cf4707c1f2ef8dc1aaa
Paul Vasemagi <vaskpaul@users.noreply.github.com>
2017-01-11 19:14:10 +0200

    masha leht algus

--- masha.php
+++ masha.php
@@ -1,1 +1,32 @@
-1
+<?php 
+
+	require("functions.php");
+	if(!isset ($_SESSION["userId"])) {
+		header("Location: login.php");
+		exit();
+	}
+	if (isset($_GET["logout"])) {
+		session_destroy();
+
+		header("Location: login.php");
+		exit();
+	}
+	
+	if (	isset($_POST["nimi"]) && 
+			isset($_POST["tagasiside"]) && 
+			!empty($_POST["nimi"]) && 
+			!empty($_POST["tagasiside"]) 
+	) {
+		
+		$nimi = cleanInput($_POST["nimi"]);
+		$tagasiside = cleanInput($_POST["tagasiside"]);
+		
+		Masha($_POST["nimi"], $_POST["tagasiside"]);
+		
+	}
+	$mashad = getMasha();
+	//echo "<pre>";
+	//var_dump($notes);
+	//echo "</pre>";
+
+?>
